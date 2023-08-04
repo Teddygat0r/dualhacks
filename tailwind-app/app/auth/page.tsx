7c68c3e64cd5da90437d4dcd9bee7f65c513cade
Ryan Mar <Phyre205@gmail.com>
2023-08-03 17:26:08 -0700

    sync database for user creation

--- tailwind-app/app/auth/page.tsx
+++ tailwind-app/app/auth/page.tsx
@@ -1,14 +1,39 @@
 "use client";
-import { auth, googleProvider } from "@/firebase/firebase";
-import { signInWithPopup, signOut } from "firebase/auth";
+import { auth, googleProvider, firestore } from "@/firebase/firebase";
+import {
+    signInWithPopup,
+    signOut,
+    getAdditionalUserInfo,
+    UserCredential,
+    AdditionalUserInfo,
+} from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { setDoc, doc } from "firebase/firestore";
 
 export default function Auth() {
     const [user] = useAuthState(auth);
 
+    const createUserObjectIfNotExists = async (result: UserCredential) => {
+        const res = getAdditionalUserInfo(result);
+        if (res === null) {
+            return;
+        }
+        const { isNewUser }: AdditionalUserInfo = res;
+        if (isNewUser) {
+            await setDoc(doc(firestore, "users", result.user.uid), {
+                username: result.user.displayName,
+                email: result.user.email,
+                classes_att: [],
+                classes_run: [],
+                profile_picture: result.user.photoURL,
+            });
+        }
+    };
+
     const signInWithGoogle = async () => {
         try {
-            await signInWithPopup(auth, googleProvider);
+            const result = await signInWithPopup(auth, googleProvider);
+            await createUserObjectIfNotExists(result);
         } catch (err) {
             console.error(err);
         }
