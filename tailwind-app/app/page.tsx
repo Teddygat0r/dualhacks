a3cac1dd925ea8b4336e088cf8cef6dadecacb6b
Happygator <perng.arthur@gmail.com>
2023-08-04 11:55:29 -0700

    Created Buttons and added blurbs for the home page
    The name is kinda cringe though

--- tailwind-app/app/page.tsx
+++ tailwind-app/app/page.tsx
@@ -1,6 +1,7 @@
 import Logo from "./Components/logo";
 import Image from "next/image";
 export default function Home() {
+    // joshua probably isn't going to look at the code that means i can say he's bald and he won't know
     return (
         <>
             <main className="flex flex-col flex-initial min-h-screen p-24">
@@ -8,18 +9,37 @@ export default function Home() {
                     <div className="w-1/2">
                         <h1>
                             <b className="text-[60px]">
-                                Help I dont know what im doing
+                                Empowering Education with AI-Assisted Learning
+                                Management
                             </b>
                         </h1>
                         <div className="text-xl">
-                            also some more text here?
-                            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa
+                            Introducing LearnMentor Connect, the revolutionary
+                            educational tool that combines the power of AI with
+                            traditional teaching methods. Designed to enhance
+                            the teaching and learning experience, LearnMentor
+                            Connect brings together educators, students, and
+                            Language Models (LLMs) in an innovative platform.
+                            Seamlessly integrated with familiar features of
+                            learning management systems, LearnMentor Connect
+                            offers personalized assignment creation assistance
+                            for teachers and comprehensive assignment
+                            explanations for students.
+                        </div>
+                        <div>
+                        <a href="/auth" className="mt-4 inline-block px-4 py-2 bg-white text-black-500 border border-blue-500 rounded-lg hover:bg-blue-100 hover:border-blue-600 transition duration-300">Log In</a>
+                            <a
+                                href="/auth"
+                                className="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300 text-right"
+                            >
+                                Sign Up
+                            </a>
                         </div>
                     </div>
                     <div className="w-1/2 text-right">
                         <Image
-                            height="400"
-                            width="400"
+                            height="300"
+                            width="300"
                             alt="no logo joever"
                             src="/demoimage.jpg"
                             className="w-full"
