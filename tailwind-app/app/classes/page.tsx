65f326214d0773ca3f0f0fec82e3617d7c29c603
Teddygat0r <zhy0224@gmail.com>
2023-08-05 16:25:37 -0700

    Update page.tsx

--- tailwind-app/app/classes/page.tsx
+++ tailwind-app/app/classes/page.tsx
@@ -4,37 +4,15 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase/firebase";
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
-import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
-import { firestore } from "@/firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
 
 export default function Page() {
-    const [user, loading, error] = useAuthState(auth);
-    const [userObj, setUserObj] = useState({});
-
-    const [snapshot, userLoading] = useDocumentDataOnce(
-        doc(firestore, "user", auth.currentUser?.uid ?? "0"),
-    );
+    const [user, loading] = useAuthState(auth);
 
     useEffect(() => {
         if (!loading && !user) {
             redirect("/");
-        } else if (user) {
-            const fetchData = async () => {
-                const data = await getDoc(
-                    doc(firestore, "users", user?.uid ?? "0"),
-                );
-                if (data.exists()) {
-                    console.log(data.data());
-                }
-                console.log(data);
-                console.log(user?.uid ?? "0");
-            };
-            fetchData();
         }
     }, [user, loading]);
-
-    useEffect(() => {});
 
     return (
         <main className="flex flex-col flex-initial min-h-screen p-24">
